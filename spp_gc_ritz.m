d0c622a89ab576b68161e30d8d605fa64eafc935
rferrier <renaudferrier@gmail.com>
2016-09-08 16:29:13 +0200

    added the ritz filter method

--- spp_gc_ritz.m
+++ spp_gc_ritz.m
@@ -10,10 +10,12 @@ addpath(genpath('./tools'))
 E       = 70000;  % MPa : Young modulus
 nu      = 0.3;    % Poisson ratio
 fscalar = 1;      % N.mm-1 : Loading on the plate
-niter   = 10;
-precond = 1;      % 1 : Use a dual precond, 2 : use regul precond
+niter   = 28;
+precond = 1;      % 1 : Use a dual precond
 mu      = 0.;    % Regularization parameter
-br      = 0.;     % noise
+ratio   = 1e7;    % Maximal ratio (for eigenfilter)
+br      = 0;     % noise
+brt     = 0;   % "translation" noise
 
 % Boundary conditions
 % first index  : index of the boundary
@@ -65,7 +67,7 @@ uin = K\f;
 uref = uin(1:2*nnodes,1);
 lagr = uin(2*nnodes+1:end,1);
 
-urefb = ( 1 + br*randn(2*nnodes,1) ) .* uref;
+urefb = ( 1 + brt + br*randn(2*nnodes,1) ) .* uref;
 
 sigma = stress(uref,E,nu,nodes,elements,order,1,ntoelem);
 plotGMSH({uref,'Vect_U';sigma,'stress'}, elements, nodes, 'reference');
@@ -111,91 +113,6 @@ dirichlet2d = [1,1,0;1,2,0;
 % K1d(indexa,indexa) = 0;
 % K2d(indexa,indexa) = 0;
 
-%% Schur operators
-% First problem
-dirichlet1s = [3,1,0;3,2,0;
-               4,1,0;4,2,0;
-               1,1,0;1,2,0];
-[K1s,C1s,nbloq1s,node2c1s,c2node1s] =...
-    Krig (nodes,elements,E,nu,order,boundaryp1,dirichlet1s);
-% Second problem
-dirichlet2s = [1,1,0;1,2,0;
-               3,1,0;3,2,0];
-[K2s,C2s,nbloq2s,node2c2s,c2node2s] =...
-    Krig (nodes,elements,E,nu,order,boundaryp2,dirichlet2s);
-%
-f11 = dirichletRhs2( urefb, 1, c2node1s, boundary, nnodes );
-f12 = dirichletRhs2( urefb, 2, c2node1s, boundary, nnodes );
-f1 = assembleDirichlet( [f11,f12] );
-f2 = loading(nbloq2s,nodes,boundary,neumann2);
-% 
-Krr = Kinter([2*b2node4-1;2*b2node4], [2*b2node4-1;2*b2node4]);
-%
-Kjj = Kinter;
-Kjj([2*b2node1-1;2*b2node1;2*b2node3-1;2*b2node3;2*b2node2-1;2*b2node2;...
-    2*b2node4-1;2*b2node4],:) = [];
-Kjj(:,[2*b2node1-1;2*b2node1;2*b2node3-1;2*b2node3;2*b2node2-1;2*b2node2;...
-    2*b2node4-1;2*b2node4]) = [];
-%
-Krj = Kinter( [2*b2node4-1;2*b2node4], : );
-Krj( :, [2*b2node1-1;2*b2node1;2*b2node3-1;2*b2node3;2*b2node2-1;2*b2node2;...
-    2*b2node4-1;2*b2node4] ) = [];
-%
-Kgj = Kinter( [2*b2node2-1;2*b2node2], : );
-Kgj( :, [2*b2node1-1;2*b2node1;2*b2node3-1;2*b2node3;2*b2node2-1;2*b2node2;...
-    2*b2node4-1;2*b2node4] ) = [];
-%
-Ikjj = inv(Kjj);
-%
-SR = full(Krr - Krj*Ikjj*Krj');
-%
-K1k = Kgj*Ikjj*Krj'*inv(SR)*Krj*Ikjj*Kgj';
-%
-% [usvdr,svdr] = svd(SR);
-% [usvdjj,svdjj] = svd(full(Kjj));
-% % [usvdrjr,svdrjr] = svd(full(Krj'*SR*Krj));
-% [usvdgj,svdgj] = svd(full(Kgj));
-% % [usvdjjr,svdjjr] = svd(full(Ikjj*Krj'*SR*Krj*Ikjj));
-% [usvdrj,svdrj] = svd(full(Krj));
-% [usvdk1,svdk1] = svd(K1k);
-% %
-% hold on
-% plot(log10(diag(svdr)),'Color','red');
-% plot(log10(diag(svdjj)),'Color','green');
-% plot(log10(diag(svdgj)),'Color','yellow');
-% plot(log10(diag(svdrj)),'Color','black');
-% plot(log10(diag(svdk1)),'Color','blue');
-% legend('svd(Sr)','svd(Kjj)','svd(Kgj)','svd(Krj)','svd(Stot = Sd-Sn)')
-% figure
-% %
-% hold on
-% plot(log10(diag(svdr)),'Color','red');
-% plot(log10(diag(svdrjr)),'Color','green');
-% plot(log10(diag(svdjjr)),'Color','black');
-% plot(log10(diag(svdk1)),'Color','blue');
-% legend('svd(Sr)','svd(KrjT*SR*Krj)','svd(Ikjj*KrjT*SR*Krj*Ikjj)',...
-%     'svd(Stot = Sd-Sn)')
-% figure
-% %
-% for i=11:18
-%     u1xy = reshape(usvdk1(:,i),2,[])';
-%     hold on
-%     plot(u1xy(:,1),'Color','red')
-%     plot(u1xy(:,2),'Color','blue')
-%     legend(['VP ',num2str(i),' (x)'], ['VP ',num2str(i),' (y)'])
-%     figure
-% end
-%
-% [ S1, b1, map1 ] = schurCompL( K1s, f1, b2node2, nbloq1s, c2node1s );
-% [ S2, b2, map2 ] = schurCompL( K2s, f2, b2node2, nbloq2s, c2node2s );
-% Stot = S1-S2;
-% btot = b1-b2;
-
-% hold on;
-% plot(log10(svd(S1-S2)),'Color','red');
-plot(log10(svd(K1k)),'Color','blue');
-figure
-% cond(K1k)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Conjugate Gradient for the problem : (S10-S20) x = S2-S1
 Itere = zeros( 2*nnodes, 1 );
@@ -203,8 +120,10 @@ d     = zeros( 2*nnodes, niter+1 );
 Ad    = zeros( 2*nnodes, niter+1 );
 Res   = zeros( 2*nnodes, niter+1 );
 Zed   = zeros( 2*nnodes, niter+1 );
-H12   = H1demi(size(Res,1), nodes, boundary, 2 );
-Mgr   = Mgrad(size(Res,1), nodes, boundary, 2 );
+alpha = zeros( niter+1, 1 );
+beta  = zeros( niter+1, 1 );
+%H12   = H1demi(size(Res,1), nodes, boundary, 2 );
+%Mgr   = Mgrad(size(Res,1), nodes, boundary, 2 );
 
 %% Perform A x0 :
 % Solve 1
@@ -241,21 +160,11 @@ Res(:,1) = b - Axz;
 
 if precond == 1
     % Solve 1
-    f1 = [Res(:,1)/2; zeros(nbloq1d,1)];
+    f1 = [Res(:,1); zeros(nbloq1d,1)];
     uin1 = K1d\f1;
     u1i = uin1(1:2*nnodes,1);
     u1 = keepField( u1i, 2, boundaryp1 );
-    % Solve 2
-    f2 = [Res(:,1)/2; zeros(nbloq2d,1)];
-    uin2 = K2d\f2;
-    u2i = uin2(1:2*nnodes,1);
-    u2 = keepField( u2i, 2, boundaryp2 );
-    %
-    Zed(:,1) = u1/2-u2/2;
-elseif precond == 2
-    Zed(index,1) = 1/E*H12(index,index)\Res(index,1);
-elseif precond == 3
-    Zed(index,1) = 1/E*Mgr(index,index)\Res(index,1);
+    Zed(:,1) = u1;
 else
     Zed(:,1) = Res(:,1);
 end
@@ -290,7 +199,7 @@ for iter = 1:niter
     den = (d(indexa,iter)'*Ad(indexa,iter));
     d(:,iter) = d(:,iter)/sqrt(den); Ad(:,iter) = Ad(:,iter)/sqrt(den);
     num = Res(indexa,iter)'*d(indexa,iter);
-
+    
     Itere         = Itere + d(:,iter)*num;
     Res(:,iter+1) = Res(:,iter) - Ad(:,iter)*num;
     
@@ -300,24 +209,18 @@ for iter = 1:niter
     
     if precond == 1
         % Solve 1
-        f1 = [Res(:,iter+1)/2; zeros(nbloq1d,1)];
+        f1 = [Res(:,iter+1); zeros(nbloq1d,1)];
         uin1 = K1d\f1;
         u1i = uin1(1:2*nnodes,1);
         u1 = keepField( u1i, 2, boundaryp1 );
-        % Solve 2
-        f2 = [Res(:,iter+1)/2; zeros(nbloq2d,1)];
-        uin2 = K2d\f2;
-        u2i = uin2(1:2*nnodes,1);
-        u2 = keepField( u2i, 2, boundaryp2 );
-        %
-        Zed(:,iter+1) = u1/2-u2/2;
-    elseif precond == 2
-        Zed(index,iter+1) = 1/E*H12(index,index)\Res(index,iter+1);
-    elseif precond == 3
-        Zed(index,iter+1) = 1/E*Mgr(index,index)\Res(index,iter+1);
+        Zed(:,iter+1) = u1;
     else
         Zed(:,iter+1) = Res(:,iter+1);
     end
+    
+    % Needed values for the Ritz stuff
+    alpha(iter)   = num;
+    beta(iter+1)  = -(Zed(indexa,iter+1)'*Ad(indexa,iter));
     
     %% Orthogonalization
     d(:,iter+1) = Zed(:,iter+1);
@@ -334,9 +237,107 @@ for iter = 1:niter
     
 end
 
+% The last one (for Ritz analysis)
+% Solve 1
+rhs1 = d(:,niter+1);
+f1 = dirichletRhs(rhs1, 2, C1, boundaryp1);
+uin1 = K1\f1;
+lagr1 = uin1(2*nnodes+1:end,1);
+lamb1 = lagr2forces( lagr1, C1, 2, boundaryp1 );
+% Solve 2
+rhs2 = d(:,niter+1);
+f2 = dirichletRhs(rhs2, 2, C2, boundaryp2);
+uin2 = K2\f2;
+lagr2 = uin2(2*nnodes+1:end,1);
+lamb2 = lagr2forces( lagr2, C2, 2, boundaryp2 );
+% Regularization term
+Nu = regul(d(:,niter+1), nodes, boundaryp2, 2);
+%
+Ad(:,niter+1) = mu*Nu+lamb1-lamb2;
+
+alpha(niter+1) = Res(indexa,niter+1)'*d(indexa,niter+1)/...
+                 sqrt(d(indexa,niter+1)'*Ad(indexa,niter+1));
+
+%% Ritz algo : find the Ritz elements
+V = zeros(2*nnodes, niter+1);
+H = zeros(niter+1, niter+1);
+
+% Build the matrices
+V(:,1) = Zed(:,1)/(sqrt(Res(:,1)'*Zed(:,1)));
+delta  = 1/alpha(1);
+eta    = sqrt(beta(1))/alpha(1);
+H(1,[1,2]) = [delta, eta];
+
+for i=1:niter-1
+   V(:,i+1) = (-1)^i*Zed(:,i+1)/(sqrt(Res(:,i+1)'*Zed(:,i+1)));
+   etap = eta;
+   delta = 1/alpha(i+1) + beta(i)/alpha(i);
+   eta = sqrt(beta(i+1))/alpha(i+1);
+   H(i+1,[i,i+1,i+2]) = [etap,delta,eta];
+end
+
+V(:,niter+1) = (-1)^niter*Zed(:,niter+1)/(sqrt(Res(:,niter+1)'*Zed(:,niter+1)));
+delta = 1/alpha(niter+1) + beta(niter)/alpha(niter);
+H(niter+1,[niter,niter+1]) = [eta,delta];
+
+% Compute eigenelems of the Hessenberg :
+[Q,Theta1] = eig(H);
+theta = diag(Theta1);
+Y = V*Q;
+
+%% Determine the truncature
+%ntrunc = 0;
+%for i=1:niter
+%   if theta(niter+1-i) < ratio*theta(niter+1)  % /!\ Order must be growing
+%      ntrunc = niter+1-i
+%      break;
+%   end
+%end
+%
+%% Actually do the truncature
+%chi = Y'*Itere;
+%if ntrunc > 0
+%   chi(1:ntrunc) = 0;
+%end
+
+% Determine the truncature (growing)
+ntrunc = 0;
+for i=2:niter+1
+   if theta(i) > ratio*theta(1)  % /!\ Order must be growing
+      ntrunc = i
+      break;
+   end
+end
+
+% Actually do the truncature
+chi = Y'*Itere;
+if ntrunc > 0
+   chi(ntrunc:end) = 0;
+   if precond == 1  % Remember Y'MY = 1
+      % Apply M = S1 = D1^-1
+      rhs1 = Y*chi;
+      f1 = dirichletRhs(rhs1, 2, C1, boundaryp1);
+      uin1 = K1\f1;
+      lagr1 = uin1(2*nnodes+1:end,1);
+      lamb1 = lagr2forces( lagr1, C1, 2, boundaryp1 );
+      ItereR = lamb1;
+   else
+      ItereR = Y*chi;
+   end
+else
+   ItereR = Itere;
+end
+
+hold on;
+plot(Itere(2*b2node2-1),'Color','red')
+plot(ItereR(2*b2node2-1),'Color','blue')
+plot(uref(2*b2node2-1),'Color','green')
+legend('brutal solution','filtred solution', 'reference')
+figure;
+
 hold on
-plot(log10(error),'Color','blue')
-plot(log10(residual),'Color','red')
+plot(log10(error(2:end)),'Color','blue')
+plot(log10(residual(2:end)),'Color','red')
 legend('error (log)','residual (log)')
 figure;
 % L-curve :
@@ -344,7 +345,6 @@ loglog(residual(2:end),regulari(2:end));
 figure
 %%%%
 %% Final problem : compute u
-% DN problem
 dirichlet = [4,1,0;4,2,0;
              3,1,0;3,2,0;
              1,1,0;1,2,0;
@@ -358,7 +358,24 @@ usoli = K \ (fdir4 + fdir2);
 usol = usoli(1:2*nnodes,1);
 fsol = Kinter*usol;
 
-plot(fsol(2*b2node2-1))
+% With the reduced solution
+dirichlet = [4,1,0;4,2,0;
+             3,1,0;3,2,0;
+             1,1,0;1,2,0;
+             2,1,0;2,2,0];
+neumann   = [];
+[K,C,nbloq] = Krig (nodes,elements,E,nu,order,boundary,dirichlet);
+fdir2 = dirichletRhs(ItereR, 2, C, boundary);
+fdir4 = dirichletRhs(urefb, 4, C, boundary);
+usoli = K \ (fdir4 + fdir2);
+
+usolR = usoli(1:2*nnodes,1);
+fsolR = Kinter*usolR;
+
+hold on;
+plot(fsol(2*b2node2-1),'Color','red')
+plot(fsolR(2*b2node2-1),'Color','blue')
+legend('brutal solution','filtred solution')
 
 total_error = norm(usol-uref)/norm(uref);
 % Compute stress :
